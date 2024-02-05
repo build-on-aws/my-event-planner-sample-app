36db7b28a6412195cbba349ba6ed0455c276d507
Stephen Howell <121949113+srhowell@users.noreply.github.com>
2024-02-05 18:24:00 +0000

    Update auth.ts
    Adding workshop steps solutions

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -8,14 +8,40 @@ This allows other parts of the app to call signIn() when a user logs in or out t
 Note that initially, as there is no authentication backend implemented, signing in/out is a toggle (it doesn't really authenticate the user).
 */
 import { defineStore } from 'pinia';
+import { getCurrentUser } from 'aws-amplify/auth';
+import { Hub } from 'aws-amplify/utils';
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         userAuthenticated: false,
         userId: '',
     }),
-    // add actions
+    // add action to check if the user is already signed in (and set the userId and authenticated status)
     actions: {
+        async checkPreviousUserSignedIn() {
+            try {
+                const user = await getCurrentUser();
+                this.userAuthenticated = true;
+                this.userId = user.userId;
+            }
+            catch (error) {
+                this.userAuthenticated = false;
+                this.userId = '';
+            }
+        }
     }
 });
 
+// Listen for authentication events
+Hub.listen('auth', async (data) => {
+    const store = useAuthStore();
+    switch (data.payload.event) {
+        case 'signedIn':
+            const user = await getCurrentUser();
+            store.userId = user.userId;
+          break;
+        case 'signedOut':
+            store.userId = '';
+          break;
+      }
+});  
